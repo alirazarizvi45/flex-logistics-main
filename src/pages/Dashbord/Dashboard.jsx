a97a679501075c0912722904476c78bfe6a44baa
alirazarizvi45 <rali13664@gmail.com>
2024-02-06 20:01:34 +0500

    updated

--- src/pages/Dashbord/Dashboard.jsx
+++ src/pages/Dashbord/Dashboard.jsx
@@ -1,11 +1,27 @@
-
+import { IdBack } from "./IdBack";
+import { IdFront } from "./IdFront";
+import { License } from "./License";
+import { ProfilePic } from "./ProfilePic";
+import { VehiclePicture } from "./VehiclePicture";
+import { VehicleRegistration } from "./VehicleRegistration";
 
 const Dashboard = () => {
   return (
     <>
-      Dashbord
+      {/* <IdBack />
+      <br />
+      <IdFront />
+      <br />
+      <License />
+      <br />
+      <ProfilePic />
+      <br />
+      <VehiclePicture />
+      <br />
+      <VehicleRegistration /> */}
+      <h1>Dashboard</h1>
     </>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
