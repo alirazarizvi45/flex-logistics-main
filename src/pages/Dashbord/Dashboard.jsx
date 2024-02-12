99b201ee7eadf623d96c6f0d20e5a3020267bce9
alirazarizvi45 <rali13664@gmail.com>
2024-02-12 23:21:19 +0500

    updated

--- src/pages/Dashbord/Dashboard.jsx
+++ src/pages/Dashbord/Dashboard.jsx
@@ -1,27 +1,7 @@
-import { IdBack } from "./IdBack";
-import { IdFront } from "./IdFront";
-import { License } from "./License";
-import { ProfilePic } from "./ProfilePic";
-import { VehiclePicture } from "./VehiclePicture";
-import { VehicleRegistration } from "./VehicleRegistration";
+import React from "react";
 
 const Dashboard = () => {
-  return (
-    <>
-      <IdBack />
-      <br />
-      <IdFront />
-      <br />
-      <License />
-      <br />
-      <ProfilePic />
-      <br />
-      <VehiclePicture />
-      <br />
-      <VehicleRegistration />
-      <h1>Dashboard</h1>
-    </>
-  );
+  return <div>Dashboard</div>;
 };
 
 export default Dashboard;
